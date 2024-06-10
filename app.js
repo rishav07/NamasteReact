edb28fc09cf46ca2e1e57f3eb1c27d7a89c9d956
Rishav <rishav07kumar@gmail.com>
2024-06-10 13:51:46 +0530

    added paecel concepts and production build

--- app.js
+++ app.js
@@ -1,18 +1,4 @@
-/**
- * 
- * <div id="parent">
- *  <div id ="child">
- *      <h1>I'm h1 tag</h1>
- *      <h2>I'm h2 tag</h2>
- *  </div>
- * </div>
- * 
- * ReactElement(Object) => HTML(Browser Understandards)
- */
-
-
-
-
+// code begins
 
 const parent = React.createElement("div", {id: "parent" }, [
     React.createElement("div", {id: "child" }, [
